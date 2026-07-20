1e8a1241c908a1632662db12c0459aa2a695ecdd
vivekvardhanmeesala-pixel <vivekvardhanmeesala7@gmail.com>
2026-07-20 11:42:23 +0530

    Time: 0 ms (100%), Space: 44.9 MB (12.27%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(target==nums[i]){
+                return i;
+            }
+            else if(target>nums[i]){
+                index=i+1;
+            }
+        }
+        return index;
+    }
+}
